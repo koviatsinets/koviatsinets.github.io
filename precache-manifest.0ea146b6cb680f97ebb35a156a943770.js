525bbdd2aabfc31949ac6d4b6deaf0496eb6194f
koviatsinets <koviatsinets@gmail.com>
2022-12-04 13:59:13 +0300

    Updates

---
+++ precache-manifest.0ea146b6cb680f97ebb35a156a943770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e557d710267f871f218b",
+    "url": "/store-react/static/css/main.acc42bcd.chunk.css"
+  },
+  {
+    "revision": "e557d710267f871f218b",
+    "url": "/store-react/static/js/main.e557d710.chunk.js"
+  },
+  {
+    "revision": "2c02afda2375985545ff",
+    "url": "/store-react/static/js/1.2c02afda.chunk.js"
+  },
+  {
+    "revision": "40d8f26b9b9bc13376cb",
+    "url": "/store-react/static/js/runtime~main.40d8f26b.js"
+  },
+  {
+    "revision": "2d9112a0c77b22464850b3299ebbb18a",
+    "url": "/store-react/index.html"
+  }
+];
